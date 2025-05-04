84c044b40bfaff5603d1680c9c3667a0c761c270
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-04 09:50:43 +0000

    punti 7 e 8

--- contatore-Laguda/src/app/fav/fav.component.ts
+++ contatore-Laguda/src/app/fav/fav.component.ts
@@ -1,11 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Numbers } from '../models/numbers.models';
+import {CommonModule} from "@angular/common"
 @Component({
   selector: 'app-fav',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fav.component.html',
   styleUrl: './fav.component.css'
 })
 export class FavComponent {
   @Input() numeri = new Numbers(0, 0)
+  colore: string = "white"
+  sup():boolean{
+    if(this.colore ==="white")
+    {
+      this.colore = "yellow"
+    }else
+    {
+      this.colore = "white"
+    }
+    return false
+  }
 }
